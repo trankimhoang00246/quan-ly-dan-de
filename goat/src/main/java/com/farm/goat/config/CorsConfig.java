e0d43aa61d85529762220184cc923c2d09e58ace
trankimhoang00246 <trankimhoang1710@gmail.com>
2026-04-19 10:20:50 +0700

    init

--- goat/src/main/java/com/farm/goat/config/CorsConfig.java
+++ goat/src/main/java/com/farm/goat/config/CorsConfig.java
@@ -14,8 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://localhost:5173", "http://localhost:8742")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*");
             }
         };
